0df78900e6973e56feb2cfc19925618c18ed5795
cpowell229 <156929427+cpowell229@users.noreply.github.com>
2025-04-01 13:30:29 -0400

    added a new feature where if the player presses e it will activate the pillar and change its apperance, plan to have the player activate 2 to spawn azrael.

---
+++ pillar_1_inactive.gd
@@ -0,0 +1,39 @@
+extends Area2D
+
+@onready var prompt_label = $Label
+var player_in_range = false
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
+	prompt_label.visible = false
+	print("Pillar script loaded on:", self.name)
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		print("Player entered the collision of:", self.name)
+		player_in_range = true
+		prompt_label.visible = true
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		print("Player exited the collision of:", self.name)
+		player_in_range = false
+		prompt_label.visible = false
+
+func _process(delta):
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		print("E pressed!")
+		activate_pillar()
+
+func activate_pillar():
+	print("Spawning active pillar and removing inactive pillar (", self.name, ")")
+	var active_pillar_scene = preload("res://pillar_1_active.tscn")
+	var active_pillar = active_pillar_scene.instantiate()
+
+	active_pillar.global_position = global_position
+	active_pillar.scale = Vector2(4,4)
+	get_parent().add_child(active_pillar)
+
+	print("New pillar added:", active_pillar.name, "– queue_free on", self.name)
+	queue_free()  # Should remove this *inactive* pillar
