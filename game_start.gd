df294ea232b53e351711836a1f23b7b15ab728ff
cpowell229 <156929427+cpowell229@users.noreply.github.com>
2025-04-02 12:18:23 -0400

    fixed camera zoom, and some collisions, set floating pillars to spawn in at the same time as Azrael

--- game_start.gd
+++ game_start.gd
@@ -19,9 +19,18 @@ func _on_pillar2_activated():
 func check_pillars():
 	if pillar1_activated and pillar2_activated:
 		spawn_azrael()
+		spawn_floating_pillar()
 
 func spawn_azrael():
 	var azrael_scene = preload("res://Azrael.tscn")
 	var azrael = azrael_scene.instantiate()
 	azrael.position = Vector2(400, 200)
 	add_child(azrael)
+func spawn_floating_pillar():
+	var pillars = preload("res://floating_pillar.tscn")
+	var pillar = pillars.instantiate()
+	var pillar2 = pillars.instantiate()
+	pillar.position = Vector2(-613, 174)
+	pillar2.position = Vector2(-679, 881)
+	add_child(pillar)
+	add_child(pillar2)
